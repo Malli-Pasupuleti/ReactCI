c7d1859c4c5bf4cfd39ced53b65e924bc22b5401
Malli-Pasupuleti <pasupuletinagamalleswararao444@gmail.com>
2025-07-30 10:37:11 +0530

    Fix: added base for gh-pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/ReactCI/', // 👈 Add this line for GitHub Pages (must match repo name)
   plugins: [react()],
 })
